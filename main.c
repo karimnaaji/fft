c41fd8993a861e303d9206999fc1f3e70ea9540f
Karim Naaji <karim.naaji@gmail.com>
2015-05-17 12:02:18 -0400

    testing

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "fft.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -22,31 +24,129 @@ float** malloc2d(int v, int h) {
     return m;
 }
 
-void testfft(char* filename) {
-    unsigned char *data;
+int powerOf2(unsigned int v) {
+    return v > 0 && (v & (v - 1)) == 0;
+}
+
+
+unsigned int toRGBA(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
+    return (r) | (g << 8) | (b << 16) | (a << 24);
+}
+
+void center(float** in, float** out, int length, int width) {
+    int i, j;
+
+    for(i = 0; i < length; i++) {
+        for(j = 0; j < width; j++) {
+            if(i < length / 2) {
+                if(j < width / 2) {
+                    out[i][j] = in[i + length / 2][j + width / 2];
+                } else {
+                    out[i][j] = in[i + length / 2][j - width / 2];
+                }
+            } else {
+                if(j < width / 2) {
+                    out[i][j] = in[i - length / 2][j + width / 2];
+                } else {
+                    out[i][j] = in[i - length / 2][j - width / 2];
+                }
+            }
+        }
+    }
+}
+
+void testfft(char* filename, char* spectrum) {
+    unsigned char* data;
+    unsigned int* modfft;
+    float** mr, **mg, **mb, **modulus, **mim;
     int x, y, comp;
+    int offset = 0;
 
     FILE *file = fopen(filename, "rb");
 
     if (!file) {
+        fclose(file);
         return;
     }
 
     data = stbi_load_from_file(file, &x, &y, &comp, STBI_rgb_alpha);
 
-    float** m = malloc2d(x, y);
+    if (x != y || !powerOf2(x)) {
+        printf("Warning: Image dimensions must be a power of 2\n");
+        goto error;
+    }
 
-    for (int i = 0; i < x; ++i) {
-        for (int j = 0; j < y; ++j) {
-            m[i][j] = (float) data[i + j * x];
+    mr = malloc2d(x, y);
+    mg = malloc2d(x, y);
+    mb = malloc2d(x, y);
+    modulus = malloc2d(x, y);
+
+    printf("width %d, height %d\n", x, y);
+
+    for (int j = 0; j < y; ++j) {
+        for (int i = 0; i < x; ++i) { 
+            int off = i + j * x + offset;
+            mr[i][j] = (float) data[off];
+            mg[i][j] = (float) data[off + 1];
+            mb[i][j] = (float) data[off + 2];
+            offset += comp-1;
         }
     }
 
-    free2d(m);
+    mim = malloc2d(x, y);
 
+    // perform fast fourier transform on the 2d signal (red channel)
+    fft(mr, mim, x, y);
+
+    // compute the modulus of the fourier transform
+    mod(modulus, mr, mim, x, y);
+
+    // shift the fourier transform to move the zero frequency in center
+    fftshift(modulus, x, y);
+
+    // expand the modulus (for viewing purposes)
+    mult(modulus, 100.0f, x, y); 
+
+    center(modulus, mr, x, y);
+
+    modfft = (unsigned int*) malloc(sizeof(unsigned int) * x * y * comp);
+
+    for (int j = 0; j < y; ++j) {
+        for (int i = 0; i < x; ++i) {
+            unsigned char v = (unsigned char)mr[i][j];
+            modfft[i + j * x] = toRGBA(v, v, v, 255);
+        }
+    }
+
+    int res = stbi_write_png(spectrum, x, y, comp, modfft, 0);
+
+    if (!res) {
+        printf("Error saving file\n");
+    }
+
+    free2d(modulus);
+    free2d(mim);
+    free2d(mr);
+    free2d(mg);
+    free2d(mb);
+    free(modfft);
+
+error:
     stbi_image_free(data);
+    fclose(file);
 }
 
 int main(int argc, char** argv) {
-	return 0;
+    if (argc < 2) {
+        printf("Usage: fft.out image_in [image_out]\n");
+        return EXIT_FAILURE;
+    }
+
+    if (argc == 3) {
+        testfft(argv[1], argv[2]);
+    } else {
+        testfft(argv[1], "spectrum.png");
+    }
+
+	return EXIT_SUCCESS;
 }
